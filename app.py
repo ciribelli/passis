e43325543fd1a2289da2b02b7f58dd99359b5b72
ciribelli <tiotal@gmail.com>
2023-08-10 12:27:28 -0300

    postgres is set

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, jsonify, request, Response, json
+from flask import Flask, request, Response, json
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
 import main
-
-# Criação do objeto do aplicativo Flask
+ 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/passisdb"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Rota principal (página inicial)
 @app.route('/')
@@ -36,4 +41,70 @@ def checkin():
 if __name__ == '__main__':
     app.run(debug=True)
 
+# Modelos
 
+class Checkin(db.Model):
+    __tablename__ = 'checkins'
+
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.DateTime, default=datetime.utcnow)
+    direction = db.Column(db.String(10))
+    checkin = db.Column(db.String(100))
+
+    def __init__(self, direction, checkin, data=None):
+        self.direction = direction
+        self.checkin = checkin
+        if data is None:
+            data = datetime.utcnow()
+        self.data = data
+
+
+# referencia do crud https://stackabuse.com/using-sqlalchemy-with-flask-and-postgresql/
+@app.route('/checkin', methods=['POST', 'GET'])
+def handle_checkin():
+    if request.method == 'POST':
+        if request.is_json:
+            dados = request.get_json()
+            new_checkin = Checkin(checkin=dados['checkin'], direction=dados['direction'], data=dados['data'])
+            db.session.add(new_checkin)
+            db.session.commit()
+            return {"message": f"checkin em {new_checkin.checkin} foi criado com sucesso."}
+        else:
+            return {"error": "Formato diferente de JSON"}
+
+    elif request.method == 'GET':
+        checkins = Checkin.query.all()
+        results = [
+            {
+                "data": checkin.data,
+                "direction": checkin.direction,
+                "checkin": checkin.checkin
+            } for checkin in checkins]
+
+        return {"count": len(results), "checkins": results}
+
+@app.route('/checkin/<checkin_id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_car(checkin_id):
+    checkin = Checkin.query.get_or_404(checkin_id)
+
+    if request.method == 'GET':
+        response = {
+            "direction": checkin.direction,
+            "checkin": checkin.checkin,
+            "data": checkin.data
+        }
+        return {"message": "success", "checkin": response}
+
+    elif request.method == 'PUT':
+        dados = request.get_json()
+        checkin.direction = dados['direction']
+        checkin.checkin = dados['checkin']
+        checkin.data = dados['data']
+        db.session.add(checkin)
+        db.session.commit()
+        return {"message": f"checkin {checkin.checkin} successfully updated"}
+
+    elif request.method == 'DELETE':
+        db.session.delete(checkin)
+        db.session.commit()
+        return {"message": f"Checkin {checkin.checkin} successfully deleted."}
