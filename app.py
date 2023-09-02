c4061e97fcbf1d9ec36299d3b66c0f42c48cbc53
Otavio Ciribelli <otavio.ciribelli@gmail.com>
2023-09-02 12:55:49 -0300

    usando arquivo externo singelo para envio de mensagem

--- app.py
+++ app.py
@@ -77,13 +77,11 @@ def webhook():
         # Verifica se há mensagens na solicitação
         if 'changes' in entry and entry['changes'][0]['value'].get('messages'):
             message = entry['changes'][0]['value']['messages'][0]
-            print("#############################")
-            print(message)
-            print("#############################")
-            #phone_number_id = message['metadata']['phone_number_id']
-            #print('phone_number_id')
-            phone_number_id = '116447921532317'
-            from_number = message['from']
+            phone_number_id = entry['changes'][0]['value']['metadata']['phone_number_id']
+
+            print('phone_number_id')
+            #phone_number_id = '116447921532317'
+
 
             # Verifica se há um ID de botão de resposta
             button_reply_id = message['interactive']['button_reply']['id'] if 'interactive' in message and 'button_reply' in message['interactive'] else None
