96501d281bf59b015fd1e34edf70aa271bf2465c
Otavio Ciribelli <otavio.ciribelli@gmail.com>
2023-09-03 16:39:57 -0300

    novoteste

--- send_msg.py
+++ send_msg.py
@@ -16,18 +16,22 @@ def send_wapp_msg(phone_number_id, from_number, coletor):
     response = requests.post(fb_url, json=payload, headers=headers)
 
 def send_wapp_question(phone_number_id, from_number, coletor):
-    print("enviando mensagem pergunta", coletor)
-    wapp_token = os.getenv('WHATSAPP_TOKEN')
-    fb_url = f"https://graph.facebook.com/v17.0/{phone_number_id}/messages?access_token={wapp_token}"
+    url = "https://graph.facebook.com/v17.0/116447921532317/messages"
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer EAASHYwjUYWkBO1vm06nYMEqnNfGARth4eRCveKzu6QCK1MARQIglzZCMSQiaxMQfi1B8NKFOuiX4nHxIlGHreGk8MxuME1aFzDZC4yJM2RLiDZBvQU99qpWfn5FoidnuJ7BMAFaU6HSpZCilBl8d6UQZAr6ucbeMaZBF1c39jvbQzjZCgm0oBBTi2A5IbLPvqXP'
+    }
+    # https://developers.facebook.com/docs/whatsapp/guides/interactive-messages/
     payload = json.dumps({
         "messaging_product": "whatsapp",
         "recipient_type": "individual",
-        "to": from_number,
+        "to": "5521983163900",
         "type": "interactive",
         "interactive": {
             "header": {
                 "type": "text",
-                "text": coletor
+                "text": "Sobre o sono"
             },
             "type": "button",
             "body": {
@@ -63,8 +67,60 @@ def send_wapp_question(phone_number_id, from_number, coletor):
             }
         }
     })
-    headers = {"Content-Type": "application/json"}
-    response = requests.post(fb_url, json=payload, headers=headers)
-    print(response)
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
     print(response.text)
+    #
+    # print("enviando mensagem pergunta", coletor)
+    # wapp_token = os.getenv('WHATSAPP_TOKEN')
+    # fb_url = f"https://graph.facebook.com/v17.0/{phone_number_id}/messages?access_token={wapp_token}"
+    # payload = json.dumps({
+    #     "messaging_product": "whatsapp",
+    #     "recipient_type": "individual",
+    #     "to": from_number,
+    #     "type": "interactive",
+    #     "interactive": {
+    #         "header": {
+    #             "type": "text",
+    #             "text": coletor
+    #         },
+    #         "type": "button",
+    #         "body": {
+    #             "text": "Dificuldade de acordar, sendo 1 muito fácil, 2 indiferente e 3 difícil:"
+    #         },
+    #         "footer": {  # optional
+    #             "text": ""
+    #         },
+    #         "action": {
+    #             "buttons": [
+    #                 {
+    #                     "type": "reply",
+    #                     "reply": {
+    #                         "id": "0",
+    #                         "title": "1"
+    #                     }
+    #                 },
+    #                 {
+    #                     "type": "reply",
+    #                     "reply": {
+    #                         "id": "1",
+    #                         "title": "2"
+    #                     }
+    #                 },
+    #                 {
+    #                     "type": "reply",
+    #                     "reply": {
+    #                         "id": "2",
+    #                         "title": "3"
+    #                     }
+    #                 }
+    #             ]
+    #         }
+    #     }
+    # })
+    # headers = {"Content-Type": "application/json"}
+    # response = requests.post(fb_url, json=payload, headers=headers)
+    # print(response)
+    # print(response.text)
 
