f21dd9e72f099867a3ab3ba4e6c16d5976914c22
Otavio Ciribelli <otavio.ciribelli@gmail.com>
2023-11-05 13:57:48 -0300

    minor changes in main_jogos

--- main.py
+++ main.py
@@ -3,94 +3,96 @@ from bs4 import BeautifulSoup
 import json
 import datetime
 
-# busca a data do dia do sistema
-hoje = datetime.date.today().strftime('%d-%m-%Y')
-#print(datetime.date.today() + datetime.timedelta(days=1))
-url = 'http://www.uol.com.br/esporte/futebol/central-de-jogos/'
-resposta = requests.get(url)
-text = BeautifulSoup(resposta.text, 'html.parser')
-print ("Estou vivo")
-# FILTRAGEM DE DATA
-i_dia = text.find('li', {'data-ts': hoje})
+def nucleo_jogos():
+    # busca a data do dia do sistema
+    hoje = datetime.date.today().strftime('%d-%m-%Y')
+    #print(datetime.date.today() + datetime.timedelta(days=1))
+    url = 'http://www.uol.com.br/esporte/futebol/central-de-jogos/'
+    resposta = requests.get(url)
+    text = BeautifulSoup(resposta.text, 'html.parser')
+    print ("Estou vivo")
+    # FILTRAGEM DE DATA
+    i_dia = text.find('li', {'data-ts': hoje})
 
-# filtro para JOGOES
-lista_jogoes=[]
-try: # porque nem sempre temos jogoes
-    lista_jogao = i_dia.find('div', {'class': 'match-wrap-bigGames'})
-    jogao = lista_jogao.find_all('div', {'class': "match-full match-wrapper"})
-    for i in jogao:
-        data_cfg = json.loads(i['data-cfg'])
-        try:
-            transmissao = i.find('div', {'class': 'transmitions'}).get_text().strip()
-            data_cfg.update({'transmissao': transmissao})
-        except:
-            pass
-        lista_jogoes.append(data_cfg)
-except:
-    print('nao tem jogao')
+    # filtro para JOGOES
+    lista_jogoes=[]
+    try: # porque nem sempre temos jogoes
+        lista_jogao = i_dia.find('div', {'class': 'match-wrap-bigGames'})
+        jogao = lista_jogao.find_all('div', {'class': "match-full match-wrapper"})
+        for i in jogao:
+            data_cfg = json.loads(i['data-cfg'])
+            try:
+                transmissao = i.find('div', {'class': 'transmitions'}).get_text().strip()
+                data_cfg.update({'transmissao': transmissao})
+            except:
+                pass
+            lista_jogoes.append(data_cfg)
+    except:
+        print('nao tem jogao')
 
-# filtro PARA JOGOS
-lista_jogos=[]
-try: # porque nem sempre temos jogos
-    lista_jogo = i_dia.find('div', {'class': "match-container top-games"})
-    jogo = lista_jogo.find('div', {'class': "match-wrap-content"})
-    a = jogo.find_all('div', {'class': "match-full match-wrapper"})
-    for i in a:
-        data_cfg = json.loads(i['data-cfg'])
-        try:
-            transmissao = i.find('div', {'class': 'transmitions'}).get_text().strip()
-            data_cfg.update({'transmissao': transmissao})
-        except:
-            pass
-        lista_jogos.append(data_cfg)
-except:
-    print('nao tem jogo')
+    # filtro PARA JOGOS
+    lista_jogos=[]
+    try: # porque nem sempre temos jogos
+        lista_jogo = i_dia.find('div', {'class': "match-container top-games"})
+        jogo = lista_jogo.find('div', {'class': "match-wrap-content"})
+        a = jogo.find_all('div', {'class': "match-full match-wrapper"})
+        for i in a:
+            data_cfg = json.loads(i['data-cfg'])
+            try:
+                transmissao = i.find('div', {'class': 'transmitions'}).get_text().strip()
+                data_cfg.update({'transmissao': transmissao})
+            except:
+                pass
+            lista_jogos.append(data_cfg)
+    except:
+        print('nao tem jogo')
 
-# filtro PARA JOGOS complementares
-lista_jogos_comp=[]
-try: # porque nem sempre temos jogos
-    lista_jogoc = i_dia.find('div', {'class': "match-container today-games"})
-    jogoc = lista_jogoc.find('div', {'class': "match-wrap-content"})
-    ac = jogoc.find_all('div', {'class': "match-full match-wrapper"})
-    for i in ac:
-        data_cfg = json.loads(i['data-cfg'])
-        try:
-            transmissao = i.find('div', {'class': 'transmitions'}).get_text().strip()
-            data_cfg.update({'transmissao': transmissao})
-        except:
-            pass
-        lista_jogos_comp.append(data_cfg)
-except:
-    print('nao tem jogo')
+    # filtro PARA JOGOS complementares
+    lista_jogos_comp=[]
+    try: # porque nem sempre temos jogos
+        lista_jogoc = i_dia.find('div', {'class': "match-container today-games"})
+        jogoc = lista_jogoc.find('div', {'class': "match-wrap-content"})
+        ac = jogoc.find_all('div', {'class': "match-full match-wrapper"})
+        for i in ac:
+            data_cfg = json.loads(i['data-cfg'])
+            try:
+                transmissao = i.find('div', {'class': 'transmitions'}).get_text().strip()
+                data_cfg.update({'transmissao': transmissao})
+            except:
+                pass
+            lista_jogos_comp.append(data_cfg)
+    except:
+        print('nao tem jogo')
 
-import pandas as pd
-import json
-# somo as duas listas contendo jogoes e jogos normais
-lista_completa = lista_jogoes + lista_jogos + lista_jogos_comp
+    import pandas as pd
+    import json
+    # somo as duas listas contendo jogoes e jogos normais
+    lista_completa = lista_jogoes + lista_jogos + lista_jogos_comp
 
-# Converter a lista de strings JSON em uma lista de dicionários
-lista_dicionarios = [item for item in lista_completa]
+    # Converter a lista de strings JSON em uma lista de dicionários
+    lista_dicionarios = [item for item in lista_completa]
 
-# Converter a lista de dicionários em DataFrame
-df = pd.DataFrame(lista_dicionarios)
+    # Converter a lista de dicionários em DataFrame
+    df = pd.DataFrame(lista_dicionarios)
 
-# tratamento 01: jogos sem transmissão são informados como NaN
-# neste caso, serão substituídos por os NaN serão substituídos por '-'
-df_f = df.fillna({'transmissao':'-'})
-# tratamento 02: jogos encerrados e com informação pós-jogo podem ter esses campos com informações
-# NaN em eventos já superados. Esse tratamento substitui NaN por '-'
-df_f = df_f.fillna({'encerrado':'-', 'posjogo':'-'})
+    # tratamento 01: jogos sem transmissão são informados como NaN
+    # neste caso, serão substituídos por os NaN serão substituídos por '-'
+    df_f = df.fillna({'transmissao':'-'})
+    # tratamento 02: jogos encerrados e com informação pós-jogo podem ter esses campos com informações
+    # NaN em eventos já superados. Esse tratamento substitui NaN por '-'
+    df_f = df_f.fillna({'encerrado':'-', 'posjogo':'-'})
+    return df_f
 
-
-# Uma possivel representação na interface de usuário é apresentada abaixo, fazendo-se
 # valer da extração de colunas mais importantes com a função 'loc'
 
 def get_jogos():
+    df_f  = nucleo_jogos
     df_resultante = df_f.loc[:,['time1', 'time2', 'hora', 'competicao', 'transmissao']].sort_values(by=['hora'])
     print(df_resultante)
     return df_resultante.to_json()
 
 def get_jogos_df(): # funcao teste para funcao hub
+    df_f = nucleo_jogos
     df_resultante = df_f.loc[:,['isBigGame','time1', 'time2', 'hora', 'competicao', 'transmissao']].sort_values(by=['hora'])
     saida = ''
     for index, row in df_resultante.iterrows():
@@ -109,6 +111,7 @@ def get_estadio(elem):
 
 # filtro 2 para para times específicos utilizando o dataframe completo
 def get_time(elem):
+    df_f = nucleo_jogos
     df_n = df_f.loc[(df_f.time1 == elem)|(df_f.time2 == elem)]
     return df_n.to_json()
 
@@ -117,13 +120,14 @@ def get_time(elem):
 def filtro_jogao():
     n_jogoes = 0
     try:
+        df_f = nucleo_jogos
         n_jogoes = df_f['isBigGame'].value_counts()[True]
     except:
         n_jogoes = 0
     return (n_jogoes)
 print(filtro_jogao())
 
-################### funcao twitter ################
+################### funcao X ################
 from serpapi import GoogleSearch
 
 def busca_X(perfil, token):
