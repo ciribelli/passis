90e44d743250e6b295b093e04bee6b8b19dedd71
Otavio Ciribelli <otavio.ciribelli@gmail.com>
2023-12-12 19:55:56 -0300

    change from gpt-3.5-turbo to gpt-3.5-turbo-instruct

--- context_newOpenAI_Lib.py
+++ context_newOpenAI_Lib.py
@@ -66,7 +66,7 @@ def answer_question(
         messages.append({"role": "user", "content": question})
 
         completion = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="gpt-3.5-turbo-instruct",
             messages=messages
         )
 
