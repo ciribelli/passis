f732ea39c97283e8c52363f8e36e0bff5989d26e
Otavio Ciribelli <otavio.ciribelli@gmail.com>
2023-12-13 19:31:01 -0300

    gpt-3.5-turbo-1106

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 import os
 
 import context_gpt35turbo
-import context_gpt35turboInstruct
 import main, chathub
 import matplotlib.pyplot as plt
 from datetime import datetime
@@ -502,7 +501,7 @@ def fazer_perguntas(pergunta):
             for registro in registros
         ]
         threads = Thread.query.with_entities(Thread.content).order_by(Thread.date_created.desc()).limit(4).all()
-        saida, first_item = context_gpt35turboInstruct.responde_emb(pergunta, dados, threads)
+        saida, first_item = context_gpt35turbo.responde_emb(pergunta, dados, threads)
         print (first_item, '<------------------')
         return saida, first_item
     except Exception as e:
