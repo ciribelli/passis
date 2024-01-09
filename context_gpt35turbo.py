9649413e25a3d5bad715935bbbc6c8ad1ebad008
Otavio Ciribelli <otavio.ciribelli@gmail.com>
2024-01-09 08:18:00 -0300

    get_clima as function calling test

--- context_gpt35turbo.py
+++ context_gpt35turbo.py
@@ -68,19 +68,9 @@ def answer_question(
         {
             "type": "function",
             "function": {
-                "name": "get_current_weather",
-                "description": "Get the current weather in a given location",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "location": {
-                            "type": "string",
-                            "description": "The city and state, e.g. San Francisco, CA",
-                        },
-                        "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
-                    },
-                    "required": ["location"],
-                },
+                "name": "get_clima",
+                "description": "Get the current weather without specifying a location",
+                "parameters": {},
             },
         }
     ]
