8d5a7927f37f078326fe1e26977e25de7fa4f1c3
Otavio Ciribelli <otavio.ciribelli@gmail.com>
2024-02-29 15:25:11 -0300

    test deploy login

--- app.py
+++ app.py
@@ -638,7 +638,6 @@ def plota_grafico(checkin_type, color):
         {'checkin': checkin_type, 'data': '2023-09-18', 'direction': 'in'},
         {'checkin': checkin_type, 'data': '2023-09-20', 'direction': 'out'},
         {'checkin': checkin_type, 'data': '2023-09-22', 'direction': 'in'},
-        {'checkin': checkin_type, 'data': '2023-09-23', 'direction': 'in'},
     ]
 
     # Convertendo datas de string para objetos datetime
