fec5517e8674aa430e9c2f8300d48c14e0f5074a
Otávio Ciribelli Borges <ciribelli@petrobras.com.br>
2024-06-07 08:51:22 -0300

    testing gpt-4o

--- context_gpt35turboFuncCalling.py
+++ context_gpt35turboFuncCalling.py
@@ -143,7 +143,8 @@ def answer_question(
         print("mensagens: \n", messages)
 
         completion = client.chat.completions.create(
-            model="gpt-3.5-turbo-0125",
+            #model="gpt-3.5-turbo-0125",
+            model="gpt-4o",
             messages=messages,
             tools=tools, # para chamada da funcao
             tool_choice="auto", # para chamada da funcao
