26df8cabc32d39e7382f274129deefc08e66ae76
Otávio Ciribelli Borges <ciribelli@petrobras.com.br>
2024-07-17 12:27:55 -0300

    entry -- get audio id

--- chathub.py
+++ chathub.py
@@ -112,7 +112,8 @@ def chatflow(entry):
             # arquivos de mídia tratados aqui
             print("Nem button_reply.id nem msg_body presentes.")
             # passo 1: recuperar 'tipo' e 'id' da mídia
-            id = entry['entry'][0]['changes'][0]['value']['messages'][0]['audio']['id']
+            print(entry)
+            id = entry[0]['changes'][0]['value']['messages'][0]['audio']['id']
             media_url_response = send_msg.get_url_wapp_media(id)
             print(media_url_response, ' <--------------------- url da mídia')
 
