ee4a943a8a7a7bafabc8e71314f49a975fcf4121
ciribelli_petro <ciribelli@petrobras.com.br>
2025-03-06 14:50:51 -0300

    json to context

--- chathub.py
+++ chathub.py
@@ -3,6 +3,7 @@ import main
 import send_msg
 import requests
 import os
+import json
 import app
 from datetime import datetime, timedelta
 import pytz
@@ -25,7 +26,7 @@ def envia_prompt_api(content, data_atual, hora_atual, phone_number_id, from_numb
         # 📅 registra mensagem recebida de usuario em threads📅
         input_data = '{"role": "user", "content":"' + content.replace('"', ' ') + '"}'
         app.salvar_thread(input_data, wapp_id)
-        app.salvar_prompt(prompt_final.replace('"', ' '), wapp_id)
+        app.salvar_prompt(json.dumps(prompt_final), wapp_id)
         print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> \n\n ', prompt_final)
         return coletor, link, tipo_pergunta
 def responde_usuario_salva_thread(phone_number_id, from_number, coletor):
