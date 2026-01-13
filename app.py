c5b8af696570c6c7f4fe2efdb1c03f2e9f22fd3f
ciribelli_petro <ciribelli@petrobras.com.br>
2026-01-13 10:42:37 -0300

    trying to solve timezone

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import requests
 import threading
 import pytz
 from flask_cors import CORS
+from sqlalchemy import text # necessidade para o endpoint de predicoes
 load_dotenv()
 
 app = Flask(__name__)
